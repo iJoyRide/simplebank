9a092580b1a9b9816eb1905e650d5bd45dd966f1
iJoyRide <sumfeis3@gmail.com>
2024-01-25 19:23:09 +0800

    adjusted the account.sql

--- db/query/account.sql
+++ db/query/account.sql
@@ -9,30 +9,19 @@ INSERT INTO accounts (
 
 -- name: GetAccount :one
 SELECT * FROM accounts
-WHERE id = $1 LIMIT 1;
-
--- name: GetAccountForUpdate :one
-SELECT * FROM accounts
-WHERE id = $1 LIMIT 1
-FOR NO KEY UPDATE;
+WHERE id = $1 
+LIMIT 1;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
-WHERE owner = $1
 ORDER BY id
-LIMIT $2
-OFFSET $3;
+LIMIT $1
+OFFSET $2;
 
 -- name: UpdateAccount :one
 UPDATE accounts
-SET balance = $2
+SET balance =$2
 WHERE id = $1
-RETURNING *;
-
--- name: AddAccountBalance :one
-UPDATE accounts
-SET balance = balance + sqlc.arg(amount)
-WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
